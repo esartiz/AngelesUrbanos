61bd5c5079f0107337bba5b80fad9fe543a55603
Edgar Sartiz <samuelortizdiaz@gmail.com>
2024-03-16 21:57:02 -0600

    security  issue resolved

--- app/Http/Middleware/AngelesUrbanos.php
+++ app/Http/Middleware/AngelesUrbanos.php
@@ -10,6 +10,7 @@ class AngelesUrbanos
     public function handle($request, Closure $next)
 {
     $user = 'giovanni';
+    $password = 'Rupertosky24-';
 
     if ($request->getUser() != $user || $request->getPassword() != $password) {
         $headers = ['WWW-Authenticate' => 'Basic'];
